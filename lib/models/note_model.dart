9a741f388b7a28f08c6b4e7cd839ceba097670ec
Youssef Ibrahim <yosfghjiu5@gmail.com>
2026-06-16 03:17:38 +0300

    Edit note

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String data;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.data,
+    required this.color,
+  });
+}
